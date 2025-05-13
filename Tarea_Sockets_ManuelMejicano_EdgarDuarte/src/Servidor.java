928b5e02a3f17187cad42ba05286c2e84c812457
manuel-mejicano <manuelortizmejicano100@gmail.com>
2025-05-12 23:39:03 -0600

    clase servidor

--- Tarea_Sockets_ManuelMejicano_EdgarDuarte/src/Servidor.java
+++ Tarea_Sockets_ManuelMejicano_EdgarDuarte/src/Servidor.java
@@ -1,3 +1,24 @@
-public class Servidor {
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
+public class Servidor {
+    public static final int PORT = 5000;
+    private static final ExecutorService pool = Executors.newFixedThreadPool(10);
+
+    public static void main(String[] args) {
+        System.out.println("Iniciando servidor en puerto " + PORT + "...");
+        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+            while (true) {
+                Socket clientSocket = serverSocket.accept();
+                pool.execute(new ClientHandler(clientSocket));
+            }
+        } catch (IOException e) {
+            System.err.println("Error al iniciar el servidor: " + e.getMessage());
+            pool.shutdown();
+        }
+    }
 }
+
